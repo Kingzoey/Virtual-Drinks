c7f569a8caa4ffb7a141a0b17c90616579b739e0
Dzy <315300270@qq.com>
2020-12-22 18:10:22 +0800

    增加商品界面
    新增增加商品界面

--- miniprogram/pages/addGoods/addGoods.js
+++ miniprogram/pages/addGoods/addGoods.js
@@ -5,8 +5,70 @@ Page({
    * 页面的初始数据
    */
   data: {
+    showTopTips:false,
+    goodsName:"",
+    goodsPrince:0,
+    goodsComponents:"",
+    goodsType:"",
 
+    types:["主食","菜品","饮品"],
+    typeIndex:0,
+
+    formData: {
+
+    },
+
+    rules:[{
+      name:"goodsname",
+      rules:{required:true,message:'请输入商品名字!'},
+    },{
+      name:"price",
+      rules:[{required:true,message:'请输入商品单价!'},{
+        validator:(rules,value,param,models)=>{
+          if (!/^[0-9]+$/.test(value)) {
+            return "商品价格不是有效的数字,有效范围0~999999之间"
+          }
+        }
+      }]
+    },{
+      name:"content",
+      rules:{required:true,message:'请输入配料成分!'},
+    },
+    ]
   },
+  formInputChange(e) {
+    const {field} = e.currentTarget.dataset
+    this.setData({
+        [`formData.${field}`]: e.detail.value
+    })
+},
+
+goodsTypeChange: function(e) {
+  console.log('picker goods_types 发生选择改变，携带值为', e.detail.value);
+
+  this.setData({
+      typeIndex: e.detail.value
+  })
+},
+submitForm() {
+  this.selectComponent('#form').validate((valid, errors) => {
+      console.log('valid', valid, errors)
+      if (!valid) {
+          const firstError = Object.keys(errors)
+          if (firstError.length) {
+              this.setData({
+                  error: errors[firstError[0]].message
+              })
+
+          }
+      } else {
+          wx.showToast({
+              title: '校验通过'
+          })
+          console.log(this.data.formData)
+      }
+  })
+},
 
   /**
    * 生命周期函数--监听页面加载
