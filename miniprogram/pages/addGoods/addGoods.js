75c6b02ccae8353369f83b7bfda3d3210cb3465a
Dzy <315300270@qq.com>
2020-12-22 20:24:41 +0800

    将新增商品信息传到云数据库
    商品简介输入完成后等他渲染一下 不然更新不到数据 我也不知道为什么……

--- miniprogram/pages/addGoods/addGoods.js
+++ miniprogram/pages/addGoods/addGoods.js
@@ -1,22 +1,29 @@
 // miniprogram/pages/addGoods/addGoods.js
+const db = wx.cloud.database()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     showTopTips:false,
     goodsName:"",
-    goodsPrince:0,
+    goodsPrice:0,
     goodsComponents:"",
     goodsType:"",
+    goodsInfo:"",
 
-    types:["主食","菜品","饮品"],
+    types:["饮品","菜品","主食"],
     typeIndex:0,
 
     formData: {
 
     },
+
+    picFiles:[{url: 'http://mmbiz.qpic.cn/mmbiz_png/VUIF3v9blLsicfV8ysC76e9fZzWgy8YJ2bQO58p43Lib8ncGXmuyibLY7O3hia8sWv25KCibQb7MbJW3Q7xibNzfRN7A/0',
+  }, {
+      loading: true
+  }, {
+      error: true}],
 
     rules:[{
       name:"goodsname",
@@ -36,20 +43,63 @@ Page({
     },
     ]
   },
-  formInputChange(e) {
+  // 表单输入检测并获取到 formData
+  getGoodsName(e) {
     const {field} = e.currentTarget.dataset
     this.setData({
-        [`formData.${field}`]: e.detail.value
+        [`formData.${field}`]: e.detail.value,
+        goodsName:e.detail.value
     })
 },
-
-goodsTypeChange: function(e) {
-  console.log('picker goods_types 发生选择改变，携带值为', e.detail.value);
-
+getGoodsPrice(e) {
+  const {field} = e.currentTarget.dataset
   this.setData({
-      typeIndex: e.detail.value
+      [`formData.${field}`]: e.detail.value,
+      goodsPrice:e.detail.value
   })
 },
+getGoodsComponents(e) {
+  const {field} = e.currentTarget.dataset
+  this.setData({
+      [`formData.${field}`]: e.detail.value,
+      goodsComponents:e.detail.value
+  })
+},
+// 获取商品类型
+goodsTypeChange: function(e) {
+  this.setData({
+    typeIndex: e.detail.value
+})
+  console.log('picker goods_types 发生选择改变，携带值为', e.detail.value);
+
+},
+// 获取商品简介
+getGoodsInfo: function(e) {
+	this.setData({
+	  goodsInfo:e.detail.value
+	}) 
+}, 
+
+//上传数据库？
+addToDB() {
+  db.collection('goods').add({
+    data:{
+      goodsName:this.data.goodsName,
+      goodsPrice:this.data.goodsPrice,
+      goodsComponents:this.data.goodsComponents,
+      goodsInfo:this.data.goodsInfo,
+      goodsType:this.data.types[this.data.typeIndex]
+    },
+    success:res=>{
+      console.log(res)
+      wx.showToast({
+        title: '成功！',
+      })
+    }
+  })
+},
+
+// 点击按钮 提交表单
 submitForm() {
   this.selectComponent('#form').validate((valid, errors) => {
       console.log('valid', valid, errors)
@@ -62,67 +112,38 @@ submitForm() {
 
           }
       } else {
-          wx.showToast({
-              title: '校验通过'
-          })
-          console.log(this.data.formData)
+        wx.showToast({
+            title: '发布成功'
+        })
+        this.addToDB()
+          
       }
   })
 },
-
+// 图片上传处理
+selectFile(files) {
+  console.log('files', files)
+  // 返回false可以阻止某次文件上传
+},
+uploadFile(files) {
+  console.log('upload files', files)
+  // 文件上传的函数，返回一个promise
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          reject('some error')
+      }, 1000)
+  })
+},
+uploadError(e) {
+  console.log('upload error', e.detail)
+},
+uploadSuccess(e) {
+  console.log('upload success', e.detail)
+},
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
